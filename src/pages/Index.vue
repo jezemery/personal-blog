900a60ad4171caf6ea5d19b2c939005c53205b3d
Jez Emery <jez@bmssalvage.co.uk>
2021-06-23 14:54:24 +0100

    Fix issue causing build to fail

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,11 +14,6 @@ query Posts {
     edges {
       node {
         id
-        date (format: "YYYY")
-        title
-        categories
-        thumbnail (quality: 90)
-        path
       }
     }
   },
