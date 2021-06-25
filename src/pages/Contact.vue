6ba614c84b6962fcea62d45194dcb5a407bba8f1
Jez Emery <jez@bmssalvage.co.uk>
2021-06-25 11:09:27 +0100

    Tidy up a few final issues

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,29 +6,12 @@
         <h1 class="contact-title">Say hi!</h1>
         <p>Leave me a note with any questions you might have, I'll get back to you as soon as possible.</p>
       </div>
-
-      <form class="contact-form" name="contact">
-
-        <div class="sender-info">
-          <div>
-            <label for="name" class="label">Your name</label>
-            <input type="text" name="name" />
-          </div>
-          <div>
-            <label for="email" class="label">Your email</label>
-            <input type="email" name="email" />
-          </div>
-        </div>
-
-        <div class="message">
-          <label for="message" class="label">Message</label>
-          <textarea name="message"></textarea>
-        </div>
-
-        <button class="button">Submit form</button>
-
-      </form>
-
+      <ul>
+        <li><a href="https://www.github.com/jezemery/" target="_blank">Github</a></li>
+        <li><a href="https://uk.linkedin.com/in/jeremy-emery" target="_blank">Linkedin</a></li>
+        <li><a href="mailto:consulting@hiohzo.com" target="_blank">Mail</a></li>
+      </ul>
+      
     </div>
   </Layout>
 </template>
