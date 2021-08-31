62a990fa2964e953c5b285b1dbd83e71fbcf8b16
Jez Emery <jez@bmssalvage.co.uk>
2021-08-31 15:55:53 +0100

    Try and add prismjs highlighting

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,28 +2,31 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Hiohzo.com",
-  siteUrl: `https://www.hiohzo.com`,
-  host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Technical Consultant",
-  plugins: [
-    {
-      use: "@gridsome/source-filesystem",
-      options: {
-        path: "journal/**/*.md",
-        typeName: "JournalPost",
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+    siteName: "Hiohzo.com",
+    siteUrl: `https://www.hiohzo.com`,
+    host: "0.0.0.0",
+    titleTemplate: "%s",
+    siteDescription: "Technical Consultant",
+    plugins: [
+        {
+            use: "@gridsome/source-filesystem",
+            options: {
+                path: "journal/**/*.md",
+                typeName: "JournalPost",
+                resolveAbsolutePaths: true,
+                remark: {
+                    externalLinksTarget: "_blank",
+                    externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+                }
+            }
         }
-      }
+    ],
+    transformers: {
+        remark: {
+            plugins: [
+                "@gridsome/remark-prismjs",
+                '@gridsome/remark-prismjs'
+            ]
+        }
     }
-  ],
-  transformers: {
-    remark: {
-      plugins: ["@gridsome/remark-prismjs"]
-    }
-  }
 };
